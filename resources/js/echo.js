33509437609ebef41cef9b1d0fa9d5261725121e
Nameraid <n3mr1d@gmail.com>
2026-06-15 07:44:31 +0700

    add new website create for publisher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,12 +3,15 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+const host = import.meta.env.VITE_REVERB_HOST;
+const wsHost = (host === 'localhost' || !host) ? window.location.hostname : host;
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsHost: wsHost,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https' || window.location.protocol === 'https:',
     enabledTransports: ['ws', 'wss'],
 });
